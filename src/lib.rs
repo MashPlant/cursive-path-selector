e1eaad71410c9f31c8eb007b66f7997808adb83a
MashPlant <740678788@qq.com>
2020-01-19 11:36:31 +0800

    split to 2 files; small modification

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,31 @@
-use std::{path::*, cell::*, rc::*, time::*, io, env, fs};
-use cursive::{view::*, views::*, event::*, theme::*};
-use cursive::utils::markup::StyledString;
+mod util;
 
+use std::{path::*, time::*, io, env, fs};
+use cursive::{view::*, views::*, event::*, theme::*, utils::markup::StyledString};
+use util::*;
+
+/// A cursive view that can display directories and allow user to select a path in the file system.
+///
+/// Double click to enter a sub directory.
 pub struct PathSelector {
   current_dir: PathBuf,
   dir_content: Vec<PathBuf>,
   last_click: Option<Instant>,
   view: LinearLayout,
   // below are reference to the views inside `view`
-  content: RcView<LinearLayout>,
+  content: RcView<NonAutoScrollView<LinearLayout>>,
   err_msg: RcView<TextView>,
 }
 
 impl PathSelector {
+  /// Return a `PathSelector` instance with the current working directory on success.
+  ///
+  /// Return `Err(io::Error)` if an error occurs when reading the directory or when reading contents from it.
   pub fn new() -> io::Result<Self> {
     env::current_dir().and_then(Self::with_path)
   }
 
+  /// Like `PathSelector::new`, but open the directory that `p` specified instead of the current working directory.
   pub fn with_path(p: impl Into<PathBuf>) -> io::Result<Self> {
     let current_dir = p.into();
     let mut dir_content = Vec::<PathBuf>::new();
@@ -24,18 +33,20 @@ impl PathSelector {
       dir_content.push(d?.path().file_name().unwrap().into());
     }
     if current_dir.parent().is_some() {
+      // there is no cross-platform issue, even if there is a weird platform that doesn't use ".." to represent parent directory
+      // because we won't use ".." directly, it is just a symbol for the user of the tui program
       dir_content.push("..".into());
     }
-    dir_content.sort();
+    dir_content.sort_unstable(); // sort it, because the order of `read_dir` is not specified
     let mut content = LinearLayout::vertical();
     for p in &dir_content {
       content.add_child(FocusableTextView::new(TextView::new(p.to_string_lossy())));
     }
-    let content = RcView::new(content);
+    let content = RcView::new(NonAutoScrollView::new(content));
     let err_msg = RcView::new(TextView::empty());
     let view = LinearLayout::vertical()
       .child(Panel::new(TextView::new(current_dir.to_string_lossy())))
-      .child(content.clone().scrollable())
+      .child(content.clone())
       .child(err_msg.clone());
     let mut ret = Self { current_dir, dir_content, last_click: None, view, content, err_msg };
     if !ret.dir_content.is_empty() {
@@ -44,16 +55,22 @@ impl PathSelector {
     Ok(ret)
   }
 
+  /// Return (current opening directory, current focused sub path).
+  ///
+  /// User can use `Path::join` to join these two paths to get a full path.
+  ///
+  /// The initially focused sub path is "..".
   pub fn focused_path(&self) -> (&PathBuf, &PathBuf) {
-    (&self.current_dir, &self.dir_content[self.content.get().get_focus_index()])
+    (&self.current_dir, &self.dir_content[self.content.get().inner().get_focus_index()])
   }
 
+  // clear the style on the last selected path, and add style to the new one
   fn recolor(&mut self, old: usize, new: usize) {
     let mut content = self.content.get();
-    content.get_child_mut(old).unwrap().as_any_mut().downcast_mut::<FocusableTextView>().unwrap()
-      .inner.set_content(self.dir_content[old].to_string_lossy());
-    content.get_child_mut(new).unwrap().as_any_mut().downcast_mut::<FocusableTextView>().unwrap()
-      .inner.set_content(StyledString::styled(self.dir_content[new].to_string_lossy(), Effect::Underline));
+    content.inner().get_child_mut(old).unwrap().as_any_mut().downcast_mut::<FocusableTextView>().unwrap()
+      .get().set_content(self.dir_content[old].to_string_lossy());
+    content.inner().get_child_mut(new).unwrap().as_any_mut().downcast_mut::<FocusableTextView>().unwrap()
+      .get().set_content(StyledString::styled(self.dir_content[new].to_string_lossy(), Effect::Underline));
   }
 }
 
@@ -61,16 +78,17 @@ impl ViewWrapper for PathSelector {
   cursive::wrap_impl!(self.view: LinearLayout);
 
   fn wrap_on_event(&mut self, event: Event) -> EventResult {
-    let focus = self.content.get().get_focus_index();
+    let focus = self.content.get().inner().get_focus_index();
     match event {
       Event::Key(Key::Up) => {
-        if self.content.get().set_focus_index(focus.wrapping_sub(1)).is_ok() {
+        if self.content.get().inner().set_focus_index(focus.wrapping_sub(1)).is_ok() {
           self.recolor(focus, focus - 1);
         }
         EventResult::Consumed(None)
       }
       Event::Key(Key::Down) => {
-        if self.content.get().set_focus_index(focus + 1).is_ok() {
+        // I guess it is not possible to have integer overflow...
+        if self.content.get().inner().set_focus_index(focus + 1).is_ok() {
           self.recolor(focus, focus + 1);
         }
         EventResult::Consumed(None)
@@ -78,18 +96,20 @@ impl ViewWrapper for PathSelector {
       _ => {
         let is_press = if let Event::Mouse { event: MouseEvent::Press(_), .. } = event { true } else { false };
         let ret = self.view.on_event(event);
-        let new_focus = self.content.get().get_focus_index();
+        let new_focus = self.content.get().inner().get_focus_index();
         self.recolor(focus, new_focus);
         if is_press {
           let update = if let Some(last_click) = self.last_click {
             if last_click.elapsed() < Duration::from_millis(500) {
-              let p = if focus == 0 {
-                self.current_dir.parent().unwrap().to_path_buf() // ".."
+              let p = if let (Some(p), true) = (self.current_dir.parent(), focus == 0) {
+                // focus == 0 && self.current_dir.parent().is_some() means the selected path is ".."
+                // so go back to parent directory
+                p.to_path_buf()
               } else {
                 self.current_dir.join(&self.dir_content[focus])
               };
               match PathSelector::with_path(&p) {
-                Ok(p) => *self = p,
+                Ok(p) => *self = p, // here the previous err msg can be cleared
                 Err(e) => {
                   let msg = format!("failed to enter dir `{}`, reason: \"{}\"", p.display(), e);
                   self.err_msg.get().set_content(StyledString::styled(msg, Color::Dark(BaseColor::Red)));
@@ -104,49 +124,4 @@ impl ViewWrapper for PathSelector {
       }
     }
   }
-}
-
-struct RcView<V> { inner: Rc<RefCell<V>> }
-
-impl<V> RcView<V> {
-  fn new(v: V) -> Self { Self { inner: Rc::new(RefCell::new(v)) } }
-
-  fn get(&self) -> RefMut<V> { self.inner.borrow_mut() }
-
-  fn clone(&self) -> Self { Self { inner: self.inner.clone() } }
-}
-
-// basically copied from NamedView
-impl<V: View> ViewWrapper for RcView<V> {
-  type V = V;
-
-  fn with_view<F, R>(&self, f: F) -> Option<R> where F: FnOnce(&Self::V) -> R {
-    self.inner.try_borrow().ok().map(|v| f(&*v))
-  }
-
-  fn with_view_mut<F, R>(&mut self, f: F) -> Option<R> where F: FnOnce(&mut Self::V) -> R {
-    self.inner.try_borrow_mut().ok().map(|mut v| f(&mut *v))
-  }
-
-  fn into_inner(mut self) -> Result<Self::V, Self> where Self::V: Sized {
-    match Rc::try_unwrap(self.inner) {
-      Err(rc) => {
-        self.inner = rc;
-        Err(self)
-      }
-      Ok(cell) => Ok(cell.into_inner()),
-    }
-  }
-}
-
-struct FocusableTextView { inner: TextView }
-
-impl FocusableTextView {
-  fn new(t: TextView) -> Self { Self { inner: t } }
-}
-
-impl ViewWrapper for FocusableTextView {
-  cursive::wrap_impl!(self.inner: TextView);
-
-  fn wrap_take_focus(&mut self, _: direction::Direction) -> bool { true }
 }
---
+++ src/util.rs
@@ -0,0 +1,72 @@
+use std::{cell::*, rc::*};
+use cursive::{view::*, views::*, direction::Direction};
+
+// a wrapper of view V
+// it allows you to hold an mutable reference to a view after adding it to another view
+pub struct RcView<V> { inner: Rc<RefCell<V>> }
+
+impl<V> RcView<V> {
+  pub fn new(v: V) -> Self { Self { inner: Rc::new(RefCell::new(v)) } }
+
+  pub fn get(&self) -> RefMut<V> { self.inner.borrow_mut() }
+
+  pub fn clone(&self) -> Self { Self { inner: self.inner.clone() } }
+}
+
+// basically copied from NamedView
+impl<V: View> ViewWrapper for RcView<V> {
+  type V = V;
+
+  fn with_view<F, R>(&self, f: F) -> Option<R> where F: FnOnce(&Self::V) -> R {
+    self.inner.try_borrow().ok().map(|v| f(&*v))
+  }
+
+  fn with_view_mut<F, R>(&mut self, f: F) -> Option<R> where F: FnOnce(&mut Self::V) -> R {
+    self.inner.try_borrow_mut().ok().map(|mut v| f(&mut *v))
+  }
+
+  fn into_inner(mut self) -> Result<Self::V, Self> where Self::V: Sized {
+    match Rc::try_unwrap(self.inner) {
+      Err(rc) => {
+        self.inner = rc;
+        Err(self)
+      }
+      Ok(cell) => Ok(cell.into_inner()),
+    }
+  }
+}
+
+pub struct FocusableTextView { inner: TextView }
+
+impl FocusableTextView {
+  pub fn new(t: TextView) -> Self { Self { inner: t } }
+
+  pub fn get(&mut self) -> &mut TextView { &mut self.inner }
+}
+
+impl ViewWrapper for FocusableTextView {
+  cursive::wrap_impl!(self.inner: TextView);
+
+  fn wrap_take_focus(&mut self, _: Direction) -> bool { true }
+}
+
+pub struct NonAutoScrollView<V> { inner: ScrollView<V> }
+
+impl<V: View> NonAutoScrollView<V> {
+  pub fn new(v: V) -> Self { Self { inner: ScrollView::new(v) } }
+
+  pub fn inner(&mut self) -> &mut V { self.inner.get_inner_mut() }
+}
+
+impl<V: View> ViewWrapper for NonAutoScrollView<V> {
+  cursive::wrap_impl!(self.inner: ScrollView<V>);
+
+  fn wrap_take_focus(&mut self, source: Direction) -> bool {
+    // todo: the implementation of ScrollView::take_focus also uses `Core::is_scrolling` to judge, is this necessary?
+    self.inner().take_focus(source)
+  }
+
+  fn wrap_focus_view(&mut self, selector: &Selector<'_>) -> Result<(), ()> {
+    self.inner().focus_view(selector)
+  }
+}
